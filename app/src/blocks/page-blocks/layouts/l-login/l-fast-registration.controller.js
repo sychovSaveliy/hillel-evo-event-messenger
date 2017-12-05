80611dea31426ef9ee84eb94e46ba9bebc250fbc
Alisa Sosunova <lissasonova@gmail.com>
2017-12-05 21:47:12 +0200

    T-05-login-layout-integration

---
+++ app/src/blocks/page-blocks/layouts/l-login/l-fast-registration.controller.js
@@ -0,0 +1,22 @@
+app.controller('l-fast-registration', function($scope, $state){
+  $scope.model = {
+    btn: {
+      fb: {
+        src: 'app/img/icons/fb-little.png',
+        alt: 'Feacebook'
+      },
+      twitter: {
+        src: 'app/img/icons/twitter-little.png',
+        alt: 'Twitter'
+      },
+      google: {
+        src: 'app/img/icons/google-little.png',
+        alt: 'G+'
+      },
+      inn: {
+        src: 'app/img/icons/linked-little.png',
+        alt: 'Linkedin'
+      }
+    }
+  };
+});
