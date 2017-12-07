6d1fea70a6802012359ac208d33594d94cabecd4
Alisa Sosunova <lissasonova@gmail.com>
2017-12-07 13:10:54 +0200

    T-26-edit-gulp-file

--- app/js/config/route.config.js
+++ app/js/config/route.config.js
@@ -1,22 +1,22 @@
-app.config(function($stateProvider, $urlRouterProvider){ 
-                                                                                                                 
-    $urlRouterProvider.otherwise("/login"); 
-        
+app.config(function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise("/login");
+
     $stateProvider
-        
-        .state('main', { 
-            url: '/main', 
+
+        .state('main', {
+            url: '/main',
             views: {
                 "":{
                     templateUrl: 'app/src/page-blocks/layouts/main/l-main.html',
                     controller: 'l-main.controller'
                 },
                 "header@main": {
-                    templateUrl: 'app/src/page-blocks/layouts/main/l-header.html',  
+                    templateUrl: 'app/src/page-blocks/layouts/main/l-header.html',
                     controller: 'l-header.controller'
                 },
                 "chats@main": {
-                    templateUrl: 'app/src/page-blocks/layouts/main/l-chats.html',  
+                    templateUrl: 'app/src/page-blocks/layouts/main/l-chats.html',
                     controller: 'l-chats.controller'
                 },
                 "dialog@main": {
@@ -26,7 +26,19 @@ app.config(function($stateProvider, $urlRouterProvider){
                 "event@main": {
                     templateUrl: 'app/src/page-blocks/layouts/main/l-event.html',
                     controller: 'l-event.controller'
-                }
-            }            
+
+        .state('view-3', {
+            url: '/view-3',
+            templateUrl: 'app/src/blocks/page-blocks/layouts/...',
+            controller: 'view-3.controller'
         })
+
+});
+
+app.filter('trustUrl', function ($sce) {
+
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
+
 });
