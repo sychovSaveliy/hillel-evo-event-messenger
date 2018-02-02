ab54000d6984adb206672528c1ceb8d921fc18ec
Alisa Sosunova <lissasonova@gmail.com>
2018-02-02 23:21:13 +0200

    T-42-mock-get-events-and-chats

--- mock/routes/data.js
+++ mock/routes/data.js
@@ -1,4 +1,4 @@
-var fs = require('fs');
+let fs = require('fs');
 
 function filereader(fsRef, path) {
     return new Promise(function (resolve, reject) {
@@ -12,19 +12,49 @@ function filereader(fsRef, path) {
 }
 
 function getUser(req, res) {
-  var servicePromise = filereader(fs, './mock/api/users/'+ req.params.id +'/user_pattern/get.json');
+  let servicePromise = filereader(fs, './mock/api/users/'+ req.params.id +'/user_pattern/get.json');
 
   servicePromise
+    .then((response) =>{
+      let contactsArr = [];
+      let oneUser = {};
+      let stop = 0;
+      let length = response.user.contacts.length;
+      for (let i =0; i < length; i++){
+        let id = response.user.contacts[i].id;
+
+        let newarr = filereader(fs, './mock/api/users/'+ id +'/user_pattern/get.json');
+        newarr
+          .then((response) =>{
+            oneUser = {
+              "name": response.user.name,
+              "id": response.user.id
+            };
+            contactsArr.push(oneUser);
+            stop = stop + 1;
+            if(length === stop){
+              response.user.contacts = contactsArr;
+              return res.json(response.user);
+            }
+            return contactsArr;
+          })
+      }
+    })
+}
+
+function getEventById(req, res) {
+  let servicePromise = filereader(fs, './mock/api/events/'+ req.params.id +'/get.json');
+  servicePromise
     .then((response) => {
-      return response.user;
+      return response;
     })
     .then((response) => {
       res.json(response);
     });
 }
 
-function getEventById(req, res) {
-  var servicePromise = filereader(fs, './mock/api/events/'+ req.params.id +'/get.json');
+function getChatById(req, res) {
+  let servicePromise = filereader(fs, './mock/api/chats/'+ req.params.id +'/get.json');
   servicePromise
     .then((response) => {
       return response;
@@ -35,7 +65,7 @@ function getEventById(req, res) {
 }
 
 function postDataUserById(req, res) {
-  var servicePromise;
+  let servicePromise;
 
   servicePromise = filereader(fs, './mock/api/users/' + req.body.login +'/post.json');
   servicePromise
@@ -44,4 +74,4 @@ function postDataUserById(req, res) {
     });
 }
 
-module.exports = { getUser, postDataUserById, getEventById };
+module.exports = { getUser, postDataUserById, getEventById, getChatById };
