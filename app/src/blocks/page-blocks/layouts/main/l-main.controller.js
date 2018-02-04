985498797c52e2bb523eaafbc745b243b9a35c4a
Andrey-Trzesnevskiy <rockman06@mail.ru>
2018-02-04 14:56:03 +0200

    T-43-chats-requests-with-angular

--- app/src/blocks/page-blocks/layouts/main/l-main.controller.js
+++ app/src/blocks/page-blocks/layouts/main/l-main.controller.js
@@ -1,17 +1,10 @@
-app.controller('main', function($scope, $flowDataUser, $transferService, $window){
-    let tokenKey;
-    let _token = $transferService.getData('auth') ||
-    {[$window.localStorage.getItem('tokenKey')]: $window.localStorage.getItem('token')};
+app.controller('main', function($scope, $flowDataUser, $transferService){
 
-    for (let key in _token) {
-        tokenKey = key
-    }
+    let _token = $transferService.getData('token') || localStorage.getItem('token');
+
     $scope.main = $scope.main || {};
-
-    $window.localStorage.setItem('tokenKey', tokenKey)
-    $window.localStorage.setItem('token', _token[tokenKey]);
     $scope.getUserDataById = params => $flowDataUser.getDataUser(params)
-        .then(response => $scope.contactsArray = response,
+        .then(response => $scope.main.userData = response,
         error => $scope.errorMessage = error.info.message);
-    $scope.getUserDataById({token: _token[tokenKey]});
+    $scope.getUserDataById({token: _token});
 });
