d3f468c7f035aae4c0725a0cd70828c769ef383c
Alisa Sosunova <lissasonova@gmail.com>
2018-02-28 23:55:53 +0200

    T-46-create-handler-for-messages

--- app/src/blocks/page-blocks/layouts/main/l-footer/l-footer.controller.js
+++ app/src/blocks/page-blocks/layouts/main/l-footer/l-footer.controller.js
@@ -4,8 +4,9 @@ app.controller('l-footer.controller', function($scope, $postSendMes){
     $scope.main.message;
 
 
-      $scope.sendMesHandler = function(author, id, text, date, time){
-
+      $scope.sendMesHandler = function(author, id, text){
+        let currentTime = new Date();
+        let month = currentTime.getMonth() + 1;
         if ($scope.main.inputMes.value === "") {
         return
         }
@@ -13,12 +14,11 @@ app.controller('l-footer.controller', function($scope, $postSendMes){
           author: author,
           id: id,
           text: text,
-          date: date,
-          time: time
+          date: currentTime.getDate() + '-'+month+'-'+currentTime.getFullYear(),
+          time: currentTime.getHours() +':'+ currentTime.getMinutes() +':'+ currentTime.getSeconds()
         };
         $postSendMes.sendMes(sender)
           .then(response => {
-            console.log(response.messages);
             $scope.main.chats = response.messages;
           });
         $scope.main.inputMes.value = "";
