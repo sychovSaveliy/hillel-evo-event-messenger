fdf5c84238a4f31e0136c2685264d05839d69671
Alisa Sosunova <lissasonova@gmail.com>
2018-03-18 13:56:52 +0200

    add_new_url_backend

--- app/js/constants/app.default.js
+++ app/js/constants/app.default.js
@@ -6,8 +6,8 @@ protocol: "http",
 
 let server ={
 protocol: "http",
-  host: "localhost",
-  port: "5006",
+  host: "ec2-34-242-245-90.eu-west-1.compute.amazonaws.com",
+  port: "8080",
 };
 
-app.constant('$default', mock);
+app.constant('$default', server);
